24f943a4feebe23b1d41dbbe4a827e9efe4bdad6
Diego Sano <diegocsano@outlook.com>
2023-03-10 02:12:55 -0300

    feat(backend): add sort to list orders

--- backend/src/app/useCases/orders/listOrders.ts
+++ backend/src/app/useCases/orders/listOrders.ts
@@ -2,9 +2,13 @@ import { Request, Response } from 'express';
 
 import { Order } from '../../models/Order';
 
-export async function listOrders(req: Request, res: Response) {
+export async function listOrders(_req: Request, res: Response) {
   try {
-    const orders = await Order.find().populate('products.product');
+    const orders = await Order.find()
+      .sort({
+        createdAt: 1,
+      })
+      .populate('products.product');
 
     res.json(orders);
   } catch (error) {
