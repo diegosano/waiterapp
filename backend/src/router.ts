778b1600dfda0a5884fc79c292d1c9ae0f0cdc13
Diego Sano <diegocsano@outlook.com>
2023-03-10 02:10:09 -0300

    feat(backend): create orders list and create use cases

--- backend/src/router.ts
+++ backend/src/router.ts
@@ -7,6 +7,8 @@ import { listCategories } from './app/useCases/categories/listCategories';
 import { listProductsByCategory } from './app/useCases/categories/listProductsByCategory';
 import { createProduct } from './app/useCases/products/createProduct';
 import { listProducts } from './app/useCases/products/listProducts';
+import { listOrders } from './app/useCases/orders/listOrders';
+import { createOrder } from './app/useCases/orders/createOrder';
 
 export const router = Router();
 
@@ -28,12 +30,8 @@ router.get('/categories/:categoryId/products', listProductsByCategory);
 router.get('/products', listProducts);
 router.post('/products', upload.single('image'), createProduct);
 
-router.get('/orders', (req, res) => {
-  res.send('OK');
-});
-router.post('/orders', (req, res) => {
-  res.send('OK');
-});
+router.get('/orders', listOrders);
+router.post('/orders', createOrder);
 router.patch('/orders/:orderId', (req, res) => {
   res.send('OK');
 });
